05c1377cd13c49ad70546f79c6b8bb5cab4f7bc0
Arrangemonk <34814431+Arrangemonk@users.noreply.github.com>
2022-10-04 10:19:29 +0200

    Proof of concept stage
    is it possible to create animations with svgs where every animatable objekct is a path and had an unique id, and all paths are compatible (have the same commands)

---
+++ framer/Converters.cs
@@ -0,0 +1,184 @@
+﻿using framer;
+using System;
+using System.Globalization;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Framer
+{
+    internal class Program
+    {
+        const string idName = "id";
+        const string pathName = "path";
+        const string groupName = "g";
+        private static XNamespace nameSpace = null;
+
+
+
+        static void Main(string[] args)
+        {
+            CultureInfo ci = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+
+            Console.WriteLine("svg animator");
+            XDocument first = null;
+            var framepaths = new Dictionary<string, List<string>>();
+            //var transforms = new Dictionary<string, List<Transform>>();
+            foreach (var path in args)
+            {
+                var svg = XDocument.Load(path);
+                if (first == null)
+                {
+                    first = svg;
+                    nameSpace = first.Root.GetDefaultNamespace();
+                }
+                //convertTransforms(ref transforms, svg.Root);
+                extractPaths(ref framepaths, svg.Root);
+                //HandleElements(svg.Root, groupName, (group, id) =>
+                //{
+                //    extractPaths(ref framepaths, group, id);
+                //});
+
+            }
+            HandleElements(first.Root, pathName, (path, id) => animatePath(path, ref framepaths, id));
+
+            //HandleElements(first.Root, groupName, (group, id) =>
+            //{
+            //    foreach (var path in animpaths(group, id))
+            //    {
+            //        animatePath(path, ref framepaths,id);
+            //    }
+            //});
+            //animateTransforms(ref transforms, first.Root);
+            first.Save(args[0].Substring(0, args[0].Length - 3) + "output.svg");
+        }
+
+        private static void convertTransforms(ref Dictionary<string, List<Transform>> transforms, XElement parent)
+        {
+            var descendants = parent.Descendants().Where(elem => TransformAttributeFromElement(elem) != null).ToArray();
+            for (int i = 0; i < descendants.Length; i++)
+            {
+                var id = descendants[i].Attribute(idName)?.Value;// ?? i.ToString();
+                if (id == null)
+                    continue;
+                var attribute = TransformAttributeFromElement(descendants[i]);
+                if (attribute == null)
+                    continue;
+
+                var decomposed = new Transform(attribute.Value.Replace("matrix(", "").Replace(")", "").Split(",").Select(part => double.Parse(part)).ToArray());
+                //attribute.Value = decomposed.ToString();
+                if (!transforms.ContainsKey(id))
+                    transforms.Add(id, new List<Transform>());
+                transforms[id].Add(decomposed);
+            }
+        }
+
+        private static void animateTransforms(ref Dictionary<string, List<Transform>> transformDict, XElement parent)
+        {
+            var descendants = parent.Descendants().Where(elem => TransformAttributeFromElement(elem) != null).ToArray();
+            for (int i = 0; i < descendants.Length; i++)
+            {
+                var id = descendants[i].Attribute(idName)?.Value;// ?? i.ToString();
+                if (id == null)
+                    continue;
+                var attribute = TransformAttributeFromElement(descendants[i]);
+                if (attribute == null)
+                    continue;
+
+                if (!transformDict.ContainsKey(id))
+                {
+                    continue;
+                }
+                var transforms = transformDict[id];
+
+                foreach (var transform in Transform.possibleTransforms(transforms))
+                {
+                    descendants[i].Add(Transform.animateTransform(transform.Type, attribute.Name.LocalName, "0s", 3, transform.Values, transform.Additive));
+                }
+                //attribute.Remove();
+            }
+        }
+
+        private static XAttribute? TransformAttributeFromElement(XElement elem)
+        {
+            return elem.Attribute("gradientTransform") ??
+                   elem.Attribute("transform");
+        }
+
+        private static void HandleElements(XElement parent, string elementName, Action<XElement, string> action)
+        {
+
+            var descendants = parent.Descendants(nameSpace + elementName).ToArray();
+            for (int i = 0; i < descendants.Length; i++)
+            {
+                var id = descendants[i].Attribute(idName)?.Value;// ?? i.ToString();
+                if (id == null)
+                    continue;
+                action(descendants[i], id);
+            }
+        }
+
+        private static void animatePath(XElement path, ref Dictionary<string, List<string>> framepaths, string index,bool loop = false)
+        {
+            var anims = framepaths[index];
+            if (anims == null || anims.Count < 2 || anims.All(anim => anim.Equals(anims[0])))
+                return;
+            if(loop)
+                anims.Add(anims[0]);// hin und her
+
+            path.Add(FPath.AnimateAttribute("d", "0s", 3, anims.ToArray()));
+        }
+
+
+        private static void extractPaths(ref Dictionary<string, List<string>> framepaths, XElement parent)
+        {
+            var descendants = parent.Descendants(nameSpace + pathName).ToArray();
+            for (int i = 0; i < descendants.Length; i++)
+            {
+                var id = descendants[i].Attribute(idName)?.Value;// ?? i.ToString();
+                if (id == null)
+                    continue;
+                if (!framepaths.ContainsKey(id))
+                    framepaths.Add(id, new List<string>());
+
+                framepaths[id].Add(descendants[i].Attribute("d").Value);
+            }
+        }
+
+        private static void extractPaths(ref Dictionary<string, List<string>> framepaths, XElement parent, string parentID)
+        {
+            var descendants = parent.Descendants(nameSpace + pathName).ToArray();
+
+            for (int i = 0; i < descendants.Length; i++)
+            {
+                var id = descendants[i].Attribute(idName)?.Value ?? parentID + i;
+
+                if (!framepaths.ContainsKey(id))
+                    framepaths.Add(id, new List<string>());
+                var d = descendants[i].Attribute("d").Value;
+                if (!framepaths[id].Contains(d))
+                    framepaths[id].Add(d);
+            }
+        }
+
+        private static IEnumerable<XElement> animpaths(XElement svg, string parentID)
+        {
+            var result = new List<XElement>();
+            int index = 0;
+            foreach (var p in svg.Elements().Where(x => x.Name.LocalName == pathName))
+            {
+                var id = p.Attributes().FirstOrDefault(a => a.Name.LocalName == idName)?.Value;
+                if (id == null)
+                {
+                    id = parentID + index;
+                    p.Add(new XAttribute(idName, id));
+                }
+                result.Add(p);
+                index++;
+            }
+            return result;
+        }
+    }
+}
+
---
+++ framer/Transforms.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace framer
+{
+    public class Converters
+    {
+
+        public static string ConvertRectangles(string x, string y, string width, string height)
+        {
+            var x1 = float.Parse(x);
+            var y1 = float.Parse(y);
+            var w = float.Parse(width);
+            var h = float.Parse(height);
+
+            if (x1 < 0 || y1 < 0 || w < 0 || h < 0)
+            {
+                return "";
+            }
+            var x2 = (x1 + w).ToString();
+            var y2 = (y1 + h).ToString();
+
+            return $"M{x},{y}L{x2},{y} {x2},{y2} {x},{y2}z";
+        }
+
+        public static string convertLine(string x1, string y1, string x2, string y2)
+        {
+            if (float.Parse(x1) < 0 || float.Parse(y1) < 0 || float.Parse(x2) < 0 || float.Parse(y2) < 0)
+            {
+                return "";
+            }
+
+            return $"M{x1},{y1}L{x2},{y2}";
+        }
+
+        //public static string convertPoly(string points,string types)
+        //{
+        //    types = types ?? "polyline";
+
+        //    var pointsArr = points
+        //        /** clear redundant characters */
+        //        .Split("     ").Join("")
+        //        .trim()
+        //        .split("/\s +|,/");
+        //    var x0 = pointsArr.shift();
+        //    var y0 = pointsArr.shift();
+
+        //    var output = 'M' + x0 + ',' + y0 + 'L' + pointsArr.join(' ');
+
+        //    return types === 'polygon' ? output + 'z' : output;
+        //}
+
+        //function convertCE(cx, cy)
+        //{
+        //    function calcOuput(cx, cy, rx, ry)
+        //    {
+        //        if (cx < 0 || cy < 0 || rx <= 0 || ry <= 0)
+        //        {
+        //            return '';
+        //        }
+
+        //        var output = 'M' + (cx - rx).toString() + ',' + cy.toString();
+        //        output += 'a' + rx.toString() + ',' + ry.toString() + ' 0 1,0 ' + (2 * rx).toString() + ',0';
+        //        output += 'a' + rx.toString() + ',' + ry.toString() + ' 0 1,0' + (-2 * rx).toString() + ',0';
+
+        //        return output;
+        //    }
+
+        //    switch (arguments.length)
+        //    {
+        //        case 3:
+        //            return calcOuput(parseFloat(cx, 10), parseFloat(cy, 10), parseFloat(arguments[2], 10), parseFloat(arguments[2], 10));
+        //        case 4:
+        //            return calcOuput(parseFloat(cx, 10), parseFloat(cy, 10), parseFloat(arguments[2], 10), parseFloat(arguments[3], 10));
+        //            break;
+        //        default:
+        //            return '';
+        //    }
+        //}
+
+    }
+
+    public class FPath
+    {
+        public static XElement AnimateAttribute(string attributeName, string begin, double frameduration, string[] values) => XElement.Parse($@"
+<svg version=""1.1"" xmlns=""http://www.w3.org/2000/svg"" xmlns:xlink=""http://www.w3.org/1999/xlink"" xml:space=""preserve"" >
+<animate 
+    attributeName=""{attributeName}"" 
+    begin=""{begin}"" 
+    dur=""{frameduration * values.Length}s"" 
+    values=""{string.Join(";", values)}"" 
+    repeatCount=""indefinite"" 
+    calcMode=""spline""
+    keySplines=""{string.Join(";", Enumerable.Repeat(".5 0 .5 1", values.Length - 1))}""
+    fill=""freeze""/>
+</svg>").Descendants().First(x => x.Name.LocalName == "animate");
+
+    }
+    public class TransformAnim
+    {
+
+        public TransformAnim(string type, string[] values, bool additive)
+        {
+            Type = type;
+            Values = values;
+            Additive = additive;
+
+         }
+
+        public bool Additive { get; set; }
+        public string Type { get; set; }
+        public string[] Values { get; set; }
+    }
+    public class Transform
+    {
+        public static XElement animateTransform(string type, string attributename, string begin, double frameduration, string[] values, bool additive) => XElement.Parse($@"
+<svg version=""1.1"" xmlns=""http://www.w3.org/2000/svg"" xmlns:xlink=""http://www.w3.org/1999/xlink"" xml:space=""preserve"" >
+    <animateTransform
+      attributeName =""{attributename}""
+      attributeType=""XML""
+      begin=""{begin}""
+      dur=""{frameduration * values.Length}s"" 
+      repeatCount=""indefinite""
+      {(additive ? "additive=\"sum\"" : "")}
+      type=""{type}""
+      values=""{string.Join(";", values)}"" />
+      calcMode=""spline""
+      keySplines=""{string.Join(";", Enumerable.Repeat(".5 0 .5 1", values.Length - 1))}""
+</svg>").Descendants().First(x => x.Name.LocalName == "animateTransform");
+
+        const string format = "0.#################";
+
+        public double ScaleX { get; set; }
+        public double ScaleY { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Angle { get; set; }
+        public double SkewX { get; set; }
+        public double SkewY { get; set; }
+
+        public const string TranslateName = "translate";
+        public string Translate => $"{X.ToString(format)},{Y.ToString(format)}";
+
+        public const string RotateName = "rotate";
+        public string Rotate => $"{Angle.ToString(format)}";
+
+        public const string SkewXName = "skewX";
+        public string SkewXAnim => $"{SkewX.ToString(format)}";
+
+        public const string SkewYName = "skewY";
+        public string SkewYAnim => $"{SkewY.ToString(format)}";
+
+        public const string ScaleName = "scale";
+        public string Scale => $"{ScaleX.ToString(format)},{ ScaleY.ToString(format)}";
+
+        //https://stackoverflow.com/questions/5107134/find-the-rotation-and-skew-of-a-matrix-transformation
+        // var a = [a, b, c, d, e, f]
+        public Transform(params double[] a)
+        {
+            var angle = normalizeAngle(Math.Atan2(a[1], a[0]));
+            var denom = Math.Pow(a[0], 2) + Math.Pow(a[1], 2);
+            var scaleX = Math.Sqrt(denom);
+            var scaleY = (a[0] * a[3] - a[2] * a[1]) / scaleX;
+            var skewX = normalizeAngle(Math.Atan2(a[0] * a[2] + a[1] * a[3], denom));
+
+            Angle = clampAngle(angle / (Math.PI / 180));    // this is rotation angle in degrees
+            ScaleX = scaleX;                                    // scaleX factor  
+            ScaleY = scaleY;                                    // scaleY factor
+            SkewX = clampAngle(skewX / (Math.PI / 180));    // skewX angle degrees
+            SkewY = 0;                                          // skewY angle degrees
+            X = a[4];                                     // translation point  x
+            Y = a[5];                                        // translation point  y
+        }
+
+        private double normalizeAngle(double angle)
+        {
+            const double tau = 2.0 * Math.PI;
+            return (angle % tau);
+        }
+
+        private double clampAngle(double angle)
+        {
+            angle += 360;
+            return angle % 360;
+        }
+
+        public override string ToString()
+        {
+            List<string> result = new List<string>();
+            if (X != 0 && Y != 0)
+                result.Add($"{TranslateName}({X.ToString(format)},{Y.ToString(format)})");
+            if (Angle != 0)
+                result.Add($"{RotateName}({Angle.ToString(format)})");
+            if (ScaleX != 1 && ScaleY != 1)
+                result.Add($"{ScaleName}({ScaleX.ToString(format)},{ ScaleY.ToString(format)})");
+            if (SkewX != 0)
+                result.Add($"{SkewXName}({SkewX.ToString(format)})");
+            if (SkewY != 0)
+                result.Add($"{SkewYName}({SkewY.ToString(format)})");
+
+            return string.Join(" ", result);
+        }
+
+        public static IEnumerable<TransformAnim> possibleTransforms(IEnumerable<Transform> transforms)
+        {
+            var translatelist = new List<string>();
+            var rotatelist = new List<string>();
+            var scalelist = new List<string>();
+            var skewXlist = new List<string>();
+            var skewYlist = new List<string>();
+
+            foreach (var transform in transforms)
+            {
+                translatelist.Add(transform.Translate);
+                rotatelist.Add(transform.Rotate);
+                scalelist.Add(transform.Scale);
+                skewXlist.Add(transform.SkewXAnim);
+                skewYlist.Add(transform.SkewYAnim);
+            }
+            if (translatelist.Any(translate => !translatelist.First().Equals(translate)))
+                yield return new TransformAnim(TranslateName, translatelist.ToArray(), false);
+            if (rotatelist.Any(rotate => !rotatelist.First().Equals(rotate)))
+                yield return new TransformAnim(RotateName, rotatelist.ToArray(), true);
+            if (scalelist.Any(scale => !scalelist.First().Equals(scale)))
+                yield return new TransformAnim(ScaleName, scalelist.ToArray(), true);
+            if (skewXlist.Any(skewX => !skewXlist.First().Equals(skewX)))
+                yield return new TransformAnim(SkewXName, skewXlist.ToArray(), true);
+            if (skewYlist.Any(skewY => !skewYlist.First().Equals(skewY)))
+                yield return new TransformAnim(SkewYName, skewYlist.ToArray(), true);
+
+            yield break;
+        }
+    }
+}
